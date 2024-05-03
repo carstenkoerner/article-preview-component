5860afa76f0fb076f485dc2140b8bec07c18ac20
carstenkoerner <mail@ckoerner.dev>
2024-05-03 12:31:22 +0200

    2nd commit - new link bar version

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+function toggleTextField() {
+    var textField = document.getElementById("link-field");
+    if (textField.style.display === "none") {
+        textField.style.display = "inline-block";
+    } else {
+        textField.style.display = "none";
+    }
+}
